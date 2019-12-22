1d2ed0f325edf3232e5c8d23e742c4fa855dc23f
alpinelife37 <54160490+alpinelife37@users.noreply.github.com>
2019-12-21 17:26:44 -0800

    misc update

--- index.js
+++ index.js
@@ -2,8 +2,35 @@ const axios = require("axios");
 const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
-//const extractor = require("extractor");
+const htmlToPdf = require("electron-html-to");
 const writeFileAsync = util.promisify(fs.writeFile);
+
+function generateHTML(githubUserData) {
+  return `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  <title>Document</title>
+</head>
+<body>
+  <div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4">Hi! My name is ${githubUserData.name}</h1>
+    <p class="lead">I am from ${githubUserData.location}.</p>
+    <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
+    <ul class="list-group">
+      <li class="list-group-item">My GitHub username is ${githubUserData.githubUser}</li>
+      <li class="list-group-item">LinkedIn: ${githubUserData.linkedin}</li>
+    </ul>
+  </div>
+</div>
+</body>
+</html>`;
+}
+
 inquirer
   .prompt([
     {
@@ -21,6 +48,7 @@ inquirer
     const queryUrl = `https://api.github.com/users/${username}`;
     axios.get(queryUrl).then(function(res) {
       const a = res.data;
+      const name = a.name;
       const avatar = a.avatar_url;
       const githubUser = a.login;
       const location = a.location;
@@ -34,9 +62,27 @@ inquirer
       const queryUrlStars = `https://api.github.com/users/${username}/starred`;
       axios.get(queryUrlStars).then(function(stars) {
         const starsLength = stars.data.length;
+
+        const githubUserData = {
+          name,
+          avatar,
+          githubUser,
+          location,
+          githubLink,
+          blogLink,
+          bio,
+          repos,
+          followers,
+          following,
+          starsLength
+        };
+
+        const html = generateHTML(githubUserData);
+        console.log(html);
       });
     });
   });
+
 // promptUser()
 //   .then(function(answers) {
 //     const html = generateHTML(answers);
@@ -49,28 +95,3 @@ inquirer
 //   .catch(function(err) {
 //     console.log(err);
 //   });
-// function generateHTML(username) {
-//   return `
-// <!DOCTYPE html>
-// <html lang="en">
-// <head>
-//   <meta charset="UTF-8">
-//   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-//   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-//   <title>Document</title>
-// </head>
-// <body>
-//   <div class="jumbotron jumbotron-fluid">
-//   <div class="container">
-//     <h1 class="display-4">Hi! My name is ${answers.name}</h1>
-//     <p class="lead">I am from ${answers.location}.</p>
-//     <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
-//     <ul class="list-group">
-//       <li class="list-group-item">My GitHub username is ${answers.github}</li>
-//       <li class="list-group-item">LinkedIn: ${answers.linkedin}</li>
-//     </ul>
-//   </div>
-// </div>
-// </body>
-// </html>`;
-// }
